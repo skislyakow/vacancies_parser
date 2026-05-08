49b84c7bba971eb07c2f60dc0448020e05e95380
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-05-08 13:19:53 +0300

    show salary for python

--- parser.py
+++ parser.py
@@ -32,10 +32,31 @@ for lang in languages:
 print(result)
 print()
 
+
+def predict_rub_salary(vacancy):
+    salary = vacancy["salary"]
+    if salary is None:
+        return None
+    if salary.get("currency") != "rur":
+        return None
+
+    salary_from = salary.get("from")
+    salary_to = salary.get("to")
+
+    if salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+    elif salary_from:
+        return salary_from * 1.2
+    elif salary_to:
+        return salary_to * 0.8
+    else:
+        return None
+
+
 params_py = {"locations[]": "c_678", "q": "Python"}
 response_py = requests.get(url, params=params_py)
 response_py.raise_for_status()
 data_py = response_py.json()
 
 for vacancy in data_py["list"]:
-    print(vacancy["salary"])
+    print(predict_rub_salary(vacancy))
