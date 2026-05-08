0a3b045e1309ce8861957ecc7e2956d570bdaa51
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-05-08 08:13:12 +0300

    count vacancies for top langs

--- parser.py
+++ parser.py
@@ -1,15 +1,32 @@
 import requests
 
 
-url = "https://career.habr.com/api/frontend/vacancies"
-params_all = {"locations[]": "c_678"}
-response_all = requests.get(url, params=params_all)
-response_all.raise_for_status()
-data_all = response_all.json()
-print("Всего вакансий в Москве:", data_all["meta"]["totalResults"])
+languages = [
+    "Python",
+    "Java",
+    "JavaScript",
+    "C#",
+    "C++",
+    "PHP",
+    "Ruby",
+    "Go",
+    "Rust",
+    "Kotlin",
+    "TypeScript",
+    "Swift",
+]
 
-params_py = {"locations[]": "c_678", "q": "python"}
-response_py = requests.get(url, params_py)
-response_py.raise_for_status()
-data_py = response_py.json()
-print("Python вакансий в Москве:", data_py["meta"]["totalResults"])
+url = "https://career.habr.com/api/frontend/vacancies"
+
+base_params = {"locations[]": "c_678"}
+result = {}
+
+for lang in languages:
+    params = base_params.copy()
+    params["q"] = f"{lang}"
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+    result[lang] = data["meta"]["totalResults"]
+
+print(result)
